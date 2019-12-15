035a44db1493dc36d9358d038a7d6118691aa0d5
angotchi <58652101+angotchi@users.noreply.github.com>
2019-12-15 16:25:52 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,11 +4,11 @@ node 'master.puppet.vm' {
   include role::master_server
 }
 
-node /*web/ {
+node /^web/ {
   include role::app_server
 }
 
-node /*web/ {
+node /^web/ {
   include role::db_server
 }
 
